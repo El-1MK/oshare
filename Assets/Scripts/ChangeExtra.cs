f9b3a20f4321b9bdaaa01e3b1ce3d0baf655b135
Elouan Moquet <Elouan.Moquet@insa-rennes.fr>
2023-12-01 16:58:39 +0900

    feat: patterns and extras

---
+++ Assets/Scripts/ChangeExtra.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ChangeExtra : MonoBehaviour
+{
+    public string current = "t-shirt";
+    public SpriteRenderer renderer;
+
+    public Sprite pocketSprite;
+
+    public void changeExtra(string extra)
+    {
+
+
+        if(extra.Equals("pocket")){
+            renderer.sprite = pocketSprite;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/ChangePattern.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ChangePattern : MonoBehaviour
+{
+    public string current = "t-shirt";
+    public GameObject spriteToChange;
+    public SpriteRenderer renderer;
+
+    public Sprite plainSprite;
+    public Sprite horizStripSprite;
+    public Sprite dotsSprite;
+
+    public void changePattern(string pattern)
+    {
+        renderer = spriteToChange.GetComponent<SpriteRenderer>();
+
+        if(pattern.Equals("horiz-stripes")){
+            renderer.sprite = horizStripSprite;
+        }else if(pattern.Equals("plain")){
+            renderer.sprite = plainSprite;
+        }else if(pattern.Equals("dots")){
+            renderer.sprite = dotsSprite;
+        }
+    }
+
+    
+}
