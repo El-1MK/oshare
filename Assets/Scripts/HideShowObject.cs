b33c92361aceaed124dd7aeda84747fc6b50402a
Elouan Moquet <Elouan.Moquet@insa-rennes.fr>
2023-12-01 14:52:26 +0900

    feat: hide/show of color picker + set the buckari font fort UI

---
+++ Assets/Scripts/HideShowObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShowObject : MonoBehaviour
+{
+    public GameObject toHide;
+
+    public void hide(){
+        toHide.SetActive(false);
+    }
+
+    public void show(){
+        toHide.SetActive(true);
+    }
+}
