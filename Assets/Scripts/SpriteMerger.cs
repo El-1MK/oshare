3fe393a2b3180632409914e9b65a9312551f34aa
Elouan Moquet <Elouan.Moquet@insa-rennes.fr>
2023-12-02 13:17:33 +0900

    feat: scrollable wardrobe + pant model

---
+++ Assets/Scripts/SpriteMerger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMerger : MonoBehaviour
+{
+    [SerializeField] private Sprite[] spritesToMerge = null;
+    [SerializeField] private SpriteRenderer finalSpriteRenderer = null;
+
+    private Color color;
+
+    private void Start()
+    {
+        Merge();
+    }
+    private void Merge()
+    {
+        Resources.UnloadUnusedAssets();
+        var newText = new Texture2D(644,900);
+
+        for(int x=0; x < newText.width; x++)
+        {
+            for(int y=0;y<newText.height;y++)
+            {
+                newText.SetPixel(x,y,new Color(1,1,1,0));
+            }
+        }
+
+        for(int i=0;i<spritesToMerge.Length;i++)
+        {
+            for(int x=0; x < spritesToMerge[i].texture.width; x++)
+        {
+            for(int y=0;y<spritesToMerge[i].texture.height;y++)
+            {
+               
+                if(spritesToMerge[i].texture.GetPixel(x,y).a == 0){
+                    color = newText.GetPixel(x,y);
+
+                }else{
+                    color = spritesToMerge[i].texture.GetPixel(x,y);
+                }
+
+                newText.SetPixel(x,y,color);        
+            }
+        }
+        }
+    newText.Apply();
+    var finalSprite = Sprite.Create(newText, new Rect(0,0,newText.width,newText.height), new Vector2(0.5f,0.5f));
+    finalSprite.name = "Merged Sprite";
+    finalSpriteRenderer.sprite = finalSprite;
+
+    }
+
+}
