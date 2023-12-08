c7da27e939faee04f38af933a4798c9ed66a31df
Guillaume Leruth <guillaume.leruth@gmail.com>
2023-12-08 13:06:19 +0900

    Rating screen initialization

---
+++ Assets/Scripts/SlideMenuSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideMenuSelection : MonoBehaviour
+{
+  private int currentItem;
+
+  private void SelectItem(int _index)
+  {
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      transform.GetChild(i).gameObject.SetActive(i == _index);
+    }
+  }
+
+  public void ChangeItem(int _change)
+  {
+    currentItem += _change;
+    SelectItem(currentItem);
+  }
+}
