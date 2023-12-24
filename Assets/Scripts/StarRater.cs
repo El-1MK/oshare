eb47a7d18de6377a5102d74bc29b3c68da9e2782
Guillaume Leruth <guillaume.leruth@gmail.com>
2023-12-24 16:29:11 +0900

    feat: reimported StarRater & Selector prefabs

---
+++ Assets/Scripts/StarRater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRater : MonoBehaviour
+{
+    private int nbStars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nbStars = 0;
+        foreach (Transform child in transform) child.gameObject.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetStars(int newNbStars) {
+      transform.GetChild(nbStars).gameObject.SetActive(false);
+      nbStars = newNbStars % 5;
+      transform.GetChild(nbStars).gameObject.SetActive(true);
+    }
+}
