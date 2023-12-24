4d8e319582c7771d8888a3be412017df39b31f66
Guillaume Leruth <guillaume.leruth@gmail.com>
2023-12-24 13:10:19 +0900

    feat: implemented button to change scene/screen

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NextScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int nextSceneIndex = (currentScene.buildIndex + 1)%SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
