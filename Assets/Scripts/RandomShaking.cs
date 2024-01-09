021e4496a2792e3f4c736bc5376fb9798133bb65
Elouan Moquet <Elouan.Moquet@insa-rennes.fr>
2024-01-09 19:26:20 +0900

    feat: Shaking script V0 to connect

---
+++ Assets/Scripts/RandomShaking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShaking : MonoBehaviour
+{
+
+    public float ShakeDetectionThreshold;
+    public float MinShakeInterval;
+
+    private float sqrShakeDetectionThreshold;
+    private float timeSinceLastShake;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sqrShakeDetectionThreshold = Mathf.Pow(ShakeDetectionThreshold,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.acceleration.sqrMagnitude >= sqrShakeDetectionThreshold
+        && Time.unscaledTime >= timeSinceLastShake + MinShakeInterval)
+        {
+            //déclencher la randomization de la tenue
+            timeSinceLastShake = Time.unscaledTime;
+        }
+    }
+}
