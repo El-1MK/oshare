a2dc1633d35178a5f814bd2e552613654026d273
Guillaume Leruth <guillaume.leruth@gmail.com>
2024-01-10 15:15:17 +0900

    fix: fix rating screen on new canvas

--- Assets/Scripts/RatingScreenLoader.cs
+++ Assets/Scripts/RatingScreenLoader.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class RatingScreenLoader : MonoBehaviour
 {
+    Vector3 [] scalings = new Vector3 [] {
+        new Vector3(0.09f, 0.09f, 0f),
+        new Vector3(0.2f, 0.2f, 0f),
+        new Vector3(0.15f, 0.15f, 0f),
+        new Vector3(0.09f, 0.09f, 0f),
+        new Vector3(0.2f, 0.2f, 0f),
+        new Vector3(0.17f, 0.15f, 0f),
+        new Vector3(0.15f, 0.15f, 0f),
+    };
+
     public List<GameObject> selectors;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -19,10 +30,12 @@ public class RatingScreenLoader : MonoBehaviour
 
     public void Load()
     {
-       foreach (GameObject selector in selectors)
-       {
-          selector.GetComponent<SlideMenuSelection>().Reset();
-          selector.GetComponent<SlideMenuSelection>().CenterChildren();
-       } 
+        for (int i = 0; i < 7; i++)
+        {
+            Debug.Log(scalings[i]);
+            GameObject selector = selectors[i];
+            selector.GetComponent<SlideMenuSelection>().Reset();
+            selector.GetComponent<SlideMenuSelection>().CenterChildren(this.scalings[i]);
+        }
     }
 }
--- Assets/Scripts/SlideMenuSelection.cs
+++ Assets/Scripts/SlideMenuSelection.cs
@@ -25,17 +25,19 @@ public class SlideMenuSelection : MonoBehaviour
     {
         foreach (Transform child in transform) child.gameObject.SetActive(false);
         if (transform.childCount > 0) transform.GetChild(0).gameObject.SetActive(true);
-        Debug.Log("Selector reset with " + transform.childCount.ToString() + " children");
     }
 
-    public void CenterChildren()
+    public void CenterChildren(Vector3 scaling)
     {
         foreach (Transform child in transform)
         {
             RectTransform rect = child.GetComponent<RectTransform>();
+
             rect.anchorMin = new Vector2(0.5f, 0.5f);
             rect.anchorMax = new Vector2(0.5f, 0.5f);
-            child.localPosition = new Vector3(0, 0, 0);
+            rect.pivot = new Vector2(0.5f, 0.5f);
+            rect.localScale = scaling;
+            child.localPosition = new Vector3(0.0f, 0.0f, 0f);
         }
     }
 }
