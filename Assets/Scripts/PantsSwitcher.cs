108e7cba3ca80b1863845418275570cc8bb5bfd2
Guillaume Leruth <guillaume.leruth@gmail.com>
2024-01-16 16:19:40 +0900

    fix: layering issue fixed

---
+++ Assets/Scripts/PantsSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantsSwitcher : MonoBehaviour
+{
+    public Transform inner, outer;
+    bool state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       state = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Switch()
+    {
+      transform.SetParent(state? outer : inner);
+      transform.SetSiblingIndex(1);
+      state = !state;
+    }
+}
